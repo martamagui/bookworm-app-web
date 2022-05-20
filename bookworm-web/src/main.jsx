190537760103cc3ee6424e3c2bf5adbc717a1e8b
Marta Molina <marta.molina@live.u-tad.com>
2022-05-21 00:34:57 +0200

    Opciones de recursos

--- bookworm-web/src/main.jsx
+++ bookworm-web/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//Opciones de recursos
+/*
+https://auto-animate.formkit.com/
+https://www.youtube.com/watch?v=18Hzm7aVzOA
+https://blobanimation.com/
+
+*/
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
