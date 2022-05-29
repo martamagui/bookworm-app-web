ff38020170ce3c96c2a2f8521c146e473b522867
Marta Molina <marta.molina@live.u-tad.com>
2022-05-29 11:50:20 +0200

    Hover banner animations

--- bookworm-web/src/components/asides/LeftAside.jsx
+++ bookworm-web/src/components/asides/LeftAside.jsx
@@ -10,21 +10,23 @@ export function LeftAside() {
     return (
         <div className="aside-left w-2/12 absolute left-48">
             <div className="aside-left__pair">
-                <h4 className="aside__title text-tertiary font-bold text-xl py-3">What are you reading?</h4>
+                <h4 className="aside__title text-tertiary font-bold text-xl py-5">What are you reading?</h4>
                 <Link to="/create-review" className="aside__button w-8/12 flex flex-row justify-start items-center py-2 px-5">
                     <img src={IcPlus} alt="Additoin icon" className="" />
                     <span className="text-white px-4 text-sm">Create a new review</span>
                 </Link>
             </div>
             <div className="aside-left__pair py-10">
-                <h4 className="aside__title text-tertiary font-bold text-xl py-3">Up for something to read?</h4>
+                <h4 className="aside__title text-tertiary font-bold text-xl py-5">Up for something to read?</h4>
                 <Link to="/saved-reviews" className="aside__button w-8/12 flex flex-row justify-start items-center py-2 px-5">
                     <img src={IcBookMark} alt="Additoin icon" className="" />
                     <span className="text-white px-4 text-sm">Saved reviews</span>
                 </Link>
             </div>
-            <Link to="/search/" className="flex justify-items-start p-0 py-8">
-                <img src={WeeklyFind} alt="Banner of weekly find - Book: She became the sun" className="banner-left w-10/12" />
-            </Link>
+            <div className="py-6">
+                <Link to="/search/" className="banner__container flex justify-items-start p-0">
+                    <img src={WeeklyFind} alt="Banner of weekly find - Book: She became the sun" className="banner-left" />
+                </Link>
+            </div>
         </div>)
 }
