f0284492e974552821dfd48e32e44883b97636c2
Marta Molina <marta.molina@live.u-tad.com>
2022-05-31 23:27:21 +0200

    --wip

--- bookworm-web/src/components/TopBookCard/TopBookCard.jsx
+++ bookworm-web/src/components/TopBookCard/TopBookCard.jsx
@@ -2,6 +2,8 @@ import IcRightArrow from "../../assets/icons/ic_right_arrow.svg"
 
 export function TopBookCard({ id, reviews }) {
     const link = `${reviews[0].image}`
+    const link2 = `${reviews.length > 1 ? reviews[1].image : reviews[0].image}`
+    const link3 = `${reviews.length > 2 ? reviews[2].image : reviews[0].image}`
     return (
         <div className="top-card p-3 my-4">
             <div className="flex flex-row justify-between">
@@ -10,8 +12,8 @@ export function TopBookCard({ id, reviews }) {
             </div>
             <div className="top-card__container flex flex-row justify-between pt-1">
                 <img className="top-card__image" src={link} alt={id} />
-                <img className="top-card__image" src={link} alt={id} />
-                <img className="top-card__image" src={link} alt={id} />
+                <img className="top-card__image" src={link2} alt={id} />
+                <img className="top-card__image" src={link3} alt={id} />
             </div>
         </div>)
 }
