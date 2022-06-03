4284f1a7aebae93de558877585d1c6e334fef26a
Marta Molina <marta.molina@live.u-tad.com>
2022-06-03 20:50:24 +0200

    Logged navigation

--- bookworm-web/src/components/navigationBars/NavigationBarLanding.jsx
+++ bookworm-web/src/components/navigationBars/NavigationBarLanding.jsx
@@ -5,35 +5,34 @@ import { Link } from "react-router-dom";
 export function NavigationBarLading() {
     const [navbarOpen, setNavbarOpen] = React.useState(false);
     return (
-        <nav className="z-50">
-            <div className="bg-on-primary-container 
-             md:bg-transparent w-full flex flex-col md:flex-row justify-between items-center py-3 md:py-6 md:px-32 xl:px-48">
-                <div className="flex flex-row justify-between w-full md:w-6/12 px-12 md:px-0">
+        <nav >
+            <div className="bg-on-primary-container w-full nav__container">
+                <div className="nav__wrapper-logo">
                     <h1 className="self-center">
                         <Link to="/" className="text-2xl font-medium">
                             <span>BookWorm</span>
                         </Link>
                     </h1>
                     <button onClick={() => setNavbarOpen(!navbarOpen)} data-collapse-toggle="mobile-menu" type="button"
-                        className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-secondary hover:text-white focus:outline-none  dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
+                        className={(navbarOpen ? "bg-secondary inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden hover:text-white focus:outline-none" : "inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden hover:text-white focus:outline-none")} aria-controls="mobile-menu" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                         <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </button>
                 </div>
-                <div className={" lg:flex flex-grow justify-start md:justify-end w-full md:block md:w-auto px-8 md:px-0" + (navbarOpen ? " flex shadow-xl rounded-3xl" : " hidden")}
+                <div className={"nav__wrapper-menu" + (navbarOpen ? " flex shadow-xl rounded-3xl" : " hidden")}
                     id="mobile-menu">
-                    <ul className="flex flex-col md:flex-row mt-4 md:mt-0">
-                        <li className="py-2 md:py-0">
+                    <ul className="">
+                        <li onClick={() => setNavbarOpen(false)} className="py-2 md:py-0">
                             <Link to="/faq"><span className="py-14 md:py-0 p-3 md:font-extralight text-xl">F.A.Q</span></Link>
                         </li>
-                        <li className="py-2 md:py-0">
+                        <li onClick={() => setNavbarOpen(false)} className="py-2 md:py-0">
                             <Link to="/about"><span className="py-14 md:py-0 p-3 md:font-extralight text-xl">About us</span></Link>
                         </li>
-                        <li className="py-2 md:py-0">
+                        <li onClick={() => setNavbarOpen(false)} className="py-2 md:py-0">
                             <Link to="/contact"><span className="py-14 md:py-0 p-3 md:font-extralight text-xl">Contact</span></Link>
                         </li>
-                        <li className="py-3 md:py-0">
+                        <li onClick={() => setNavbarOpen(false)} className="py-3 md:py-0">
                             <Link className="p-2 font-light" to="/login"><span className="text-lg p-2 px-8 bg-orange-600 rounded-full" >Login</span></Link>
                         </li>
                     </ul>
--- bookworm-web/src/components/navigationBars/NavigationBarLogged.jsx
+++ bookworm-web/src/components/navigationBars/NavigationBarLogged.jsx
@@ -14,24 +14,40 @@ export function NavigationBarLogged() {
 
     const handleLogOut = () => logout().then(useNavigate('/'));
 
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+
+
     return (
-        <div className="flex flex-row justify-between items-center py-6 px-48">
-            <h1>
-                <Link to="/" className="text-2xl font-medium">
-                    <span className="text-tertiary">BookWorm</span>
-                </Link>
-            </h1>
-            <ul className="flex flex-row items-center">
-                <li>
-                    <Link to="/" className="flex flex-row"><img src={icHome} alt="Home icon" /><span className="p-3 text-xl text-tertiary">Home</span></Link>
-                </li>
-                <li className="px-3">
-                    <Link to="/my-profile" className="flex flex-row" ><img src={icProfile} alt="Profile icon" /><span className="p-4 text-xl text-tertiary">Profile</span></Link>
-                </li>
-                <li>
-                    <button onClick={handleLogOut}><span className="text-lg p-2 px-8 bg-tertiary text-white rounded-full hover:opacity-95 hover:shadow-md " >Log Out</span></button>
-                </li>
-            </ul>
-        </div>
+        <nav className="w-full">
+            <div className="w-full nav__container ">
+                <div className="nav__wrapper-logo">
+                    <h1>
+                        <Link to="/" className="text-2xl font-medium">
+                            <span className="text-tertiary ">BookWorm</span>
+                        </Link>
+                    </h1>
+                    <button onClick={() => setNavbarOpen(!navbarOpen)} data-collapse-toggle="mobile-menu" type="button"
+                        className={(navbarOpen ? "bg-tertiary inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden focus:outline-none" : "inline-flex items-center p-2 ml-3 text-sm text-tertiary rounded-lg md:hidden focus:outline-none")} aria-controls="mobile-menu" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                        <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    </button>
+                </div>
+
+                <div className={"nav__wrapper-menu" + (navbarOpen ? " flex shadow-xl rounded-3xl" : " hidden")} id="mobile-menu">
+                    <ul className=" items-center">
+                        <li>
+                            <Link to="/" className="flex flex-row"><img src={icHome} alt="Home icon" /><span className="p-3 text-xl text-tertiary">Home</span></Link>
+                        </li>
+                        <li className="px-3">
+                            <Link to="/my-profile" className="flex flex-row" ><img src={icProfile} alt="Profile icon" /><span className="p-4 text-xl text-tertiary">Profile</span></Link>
+                        </li>
+                        <li>
+                            <button onClick={handleLogOut}><span className="whitespace-nowrap text-lg p-2 px-8 bg-tertiary text-white rounded-full hover:opacity-95 hover:shadow-md " >Log Out</span></button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
